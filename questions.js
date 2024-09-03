bf60c4a8daddbb50933bfdff7b22ffe579d860bc
nhfaiza <faizanurhakim.ridley@gmail.com>
2024-09-04 00:52:39 +0800

    Initial commit
    Initial commited files

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is blablabla?",
+        answer: "B. bbbbb",
+        options: [
+            "A. aaaaa",
+            "B. bbbbb",
+            "C. ccccc",
+            "D. ddddd",
+        ]
+
+    },
+    {
+        numb: 2,
+        question: "What does aaaaa means?",
+        answer: "C. ccccc 2",
+        options: [
+            "A. aaaaa 2",
+            "B. bbbbb 2",
+            "C. ccccc 2",
+            "D. ddddd 2"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does bbbbb means?",
+        answer: "D. ddddd 3",
+        options: [
+            "A. aaaaa 3",
+            "B. bbbbb 3",
+            "C. ccccc 3",
+            "D. ddddd 3"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does ccccc means?",
+        answer: "B. bbbbb 4",
+        options: [
+            "A. aaaaa 4",
+            "B. bbbbb 4",
+            "C. ccccc 4",
+            "D. ddddd 4"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does ddddd means?",
+        answer: "A. aaaaa 5",
+        options: [
+            "A. aaaaa 5",
+            "B. bbbbb 5",
+            "C. ccccc 5",
+            "D. ddddd 5"
+        ]
+    },
+]
